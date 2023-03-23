d5cc518f1d016d266237a687c93a8fa22bfa64ae
Abbas <Abbas@gmail.com>
2023-03-24 01:44:36 +0500

    firebasedbConnection

--- detector/views.py
+++ detector/views.py
@@ -4,7 +4,7 @@ import pyrebase
 firebaseConfig = {
   "apiKey": "AIzaSyCssiNi_NCUDmaK2mg0VRWElUpVMkVs-pI",
   "authDomain": "driverdetection-64c39.firebaseapp.com",
-  "databaseURL":"https://driverdetection-64c39-default-rtdb.firebaseio.com/"
+  "databaseURL":"https://driverdetection-64c39-default-rtdb.firebaseio.com/",
   "projectId": "driverdetection-64c39",
   "storageBucket": "driverdetection-64c39.appspot.com",
   "messagingSenderId": "237746441834",
@@ -18,10 +18,10 @@ storage=firebase.storage()
 
 
 def home(request):
+
     Name=database.child('Data').child('Name').get().val()
     Copartner=database.child('Data').child('Copartner').get().val()
     Project=database.child('Data').child('Project').get().val()
-
     return render(request,'home.html',{
         "Name":Name,
         "Copartner":Copartner,
