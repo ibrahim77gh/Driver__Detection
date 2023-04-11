5976441c30eeeda69e57e9f56b4ba7d2231cd5c3
ibrahim77gh <advdriver@4146c987a9a6>
2023-04-11 11:32:01 +0000

    Settings update

--- detector/settings.py
+++ detector/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
