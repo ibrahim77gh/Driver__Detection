14c068cd36c8d6132571ee6e1402fc31c7538d7b
ibrahim77gh <syedibrahim4091@gmail.com>
2023-04-12 14:09:59 +0500

    Made Singleton

--- detector/Firebase.py
+++ detector/Firebase.py
@@ -2,17 +2,21 @@ import firebase_admin
 from firebase_admin import credentials
 
 class Firebase:
-    def __init__(self):
-        self.cred = credentials.Certificate('/home/advdriver/Driver__Detection/detector/serviceAccount.json')
-        # E:\Github repos\Driver_Detection\Driver__Detection\detector\serviceAccount.json
-        # /home/advdriver/Driver__Detection/detector/serviceAccount.json'
-        self.firebase_app = None
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._initialize_firebase()
+        return cls._instance
+
+    def _initialize_firebase(self):
+        self.cred = credentials.Certificate('E:\Github repos\Driver_Detection\Driver__Detection\detector\serviceAccount.json')
+        self.firebase_app = firebase_admin.initialize_app(self.cred, {
+            'databaseURL': 'https://adv-driver-monitoring-system-default-rtdb.asia-southeast1.firebasedatabase.app',
+            'storageBucket': 'adv-driver-monitoring-system.appspot.com',
+            'credential': f'{self.cred}'
+        })
 
     def get_app(self):
-        if not self.firebase_app:
-            self.firebase_app = firebase_admin.initialize_app(self.cred, {
-                'databaseURL': 'https://adv-driver-monitoring-system-default-rtdb.asia-southeast1.firebasedatabase.app',
-                'storageBucket': 'adv-driver-monitoring-system.appspot.com',
-                'credential': f'{self.cred}'
-            })
         return self.firebase_app
